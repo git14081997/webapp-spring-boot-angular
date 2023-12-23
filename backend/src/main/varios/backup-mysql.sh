09341e3b41637ee9627355ecc2a747b99bce2c1e
rodriguez971408 <rodriguez_140897@icloud.com>
2023-12-23 07:35:56 -0600

    Se agregan parámetros para guardar toda la info posible

--- backend/src/main/varios/backup-mysql.sh
+++ backend/src/main/varios/backup-mysql.sh
@@ -15,7 +15,7 @@ chmod 700 $DESTINO;
 
 NOMBRE_ARCHIVO="${FECHA}.db"
 
-mysqldump -h$HOST -u$USUARIO -p$PASSWORD --opt $DBNAME > ${DESTINO}/${NOMBRE_ARCHIVO};
+mysqldump  -h$HOST  -u$USUARIO  -p$PASSWORD  --events  --routines  --triggers  --default-character-set=utf8  --opt $DBNAME > ${DESTINO}/${NOMBRE_ARCHIVO};
 chmod 400 $DESTINO/*;
 
 exit 0;
