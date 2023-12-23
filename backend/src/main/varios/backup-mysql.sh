4d352021b3287e246650c8f099b3c2c26c894bbb
rodriguez971408 <rodriguez_140897@icloud.com>
2023-12-23 08:28:48 -0600

    directorio destino cambio, y se agrega extension sql

--- backend/src/main/varios/backup-mysql.sh
+++ backend/src/main/varios/backup-mysql.sh
@@ -4,17 +4,16 @@ HOST="127.0.0.1"
 PORT="3306"
 USUARIO="root"
 PASSWORD="esUnSecreto"
-DBNAME="INVENTARIO_FACTURACION"
 
 DIA=`date +"%Y-%m-%d"`
 HORA=`date +"%H-%M-%S"`
 FECHA=$DIA-$HORA
 
-DESTINO="$HOME/$DBNAME"
+DESTINO="$HOME/backup-db"
 mkdir -p $DESTINO
 chmod 700 $DESTINO;
 
-mysqldump -h$HOST -P$PORT -u$USUARIO -p$PASSWORD --events --routines --triggers --default-character-set=utf8 --opt INVENTARIO_FACTURACION > ${DESTINO}/${FECHA}-INVENTARIO_FACTURACION;
-mysqldump -h$HOST -P$PORT -u$USUARIO -p$PASSWORD --events --routines --triggers --default-character-set=utf8 --opt DBDEV                  > ${DESTINO}/${FECHA}-DBDEV;
+mysqldump -h$HOST -P$PORT -u$USUARIO -p$PASSWORD --events --routines --triggers --default-character-set=utf8 --opt INVENTARIO_FACTURACION > ${DESTINO}/${FECHA}-INVENTARIO_FACTURACION.sql;
+mysqldump -h$HOST -P$PORT -u$USUARIO -p$PASSWORD --events --routines --triggers --default-character-set=utf8 --opt DBDEV                  > ${DESTINO}/${FECHA}-DBDEV.sql;
 
 exit 0;
