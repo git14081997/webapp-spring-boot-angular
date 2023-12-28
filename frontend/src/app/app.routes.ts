e3d6e19174c176b8c1b453113abb780ebdecd3da
rodriguez971408 <rodriguez_140897@icloud.com>
2023-12-28 12:12:07 -0600

    rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,22 @@
 
 import { Routes } from '@angular/router';
+
 import { PruebasComponent } from './pruebas/pruebas.component';
+
 import { UsuariosListComponent } from './usuarios-list/usuarios-list.component';
+import { CategoriaListComponent } from './categoria-list/categoria-list.component';
+import { ClienteabonaListComponent } from './clienteabona-list/clienteabona-list.component';
+import { FacturaListComponent } from './factura-list/factura-list.component';
+import { FacturadetalleListComponent } from './facturadetalle-list/facturadetalle-list.component';
+import { ProductoListComponent } from './producto-list/producto-list.component';
+import { TipopagoListComponent } from './tipopago-list/tipopago-list.component';
 
 export const routes: Routes = [
-{ path: '', component: UsuariosListComponent, title: 'Pruebas',},
+{ path: '', component: UsuariosListComponent, title: 'Usuarios',},
+{ path: '', component: CategoriaListComponent, title: 'Categorias',},
+{ path: '', component: ClienteabonaListComponent, title: 'Cliente Abona',},
+{ path: '', component: FacturaListComponent, title: 'Facturas',},
+{ path: '', component: FacturadetalleListComponent, title: 'Detalles de facturas',},
+{ path: '', component: ProductoListComponent, title: 'Productos',},
+{ path: '', component: TipopagoListComponent, title: 'Tipos de pago',},
 ];
