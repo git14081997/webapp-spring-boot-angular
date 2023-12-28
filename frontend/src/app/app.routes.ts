21010c29e431256e94e2b25bb02b21b1ccf7fbfd
rodriguez971408 <rodriguez_140897@icloud.com>
2023-12-28 12:16:42 -0600

    rutas y componentes inicio y pag404

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -11,12 +11,20 @@ import { FacturadetalleListComponent } from './facturadetalle-list/facturadetall
 import { ProductoListComponent } from './producto-list/producto-list.component';
 import { TipopagoListComponent } from './tipopago-list/tipopago-list.component';
 
+import { InicioComponent } from './inicio/inicio.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
 export const routes: Routes = [
-{ path: '', component: UsuariosListComponent, title: 'Usuarios',},
-{ path: '', component: CategoriaListComponent, title: 'Categorias',},
-{ path: '', component: ClienteabonaListComponent, title: 'Cliente Abona',},
-{ path: '', component: FacturaListComponent, title: 'Facturas',},
-{ path: '', component: FacturadetalleListComponent, title: 'Detalles de facturas',},
-{ path: '', component: ProductoListComponent, title: 'Productos',},
-{ path: '', component: TipopagoListComponent, title: 'Tipos de pago',},
+
+{ path: 'inicio', component: InicioComponent, title: 'Inicio',},
+{ path: '', component: PagenotfoundComponent, title: 'Error 404',},
+{ path: 'usuarios', component: UsuariosListComponent, title: 'Usuarios',},
+{ path: 'categorias', component: CategoriaListComponent, title: 'Categorias',},
+{ path: 'cliabo', component: ClienteabonaListComponent, title: 'Cliente Abona',},
+{ path: 'facturas', component: FacturaListComponent, title: 'Facturas',},
+{ path: 'detalledefacturas', component: FacturadetalleListComponent, title: 'Detalles de facturas',},
+{ path: 'productos', component: ProductoListComponent, title: 'Productos',},
+{ path: 'tiposdepago', component: TipopagoListComponent, title: 'Tipos de pago',},
+{ path: 'pruebas', component: PruebasComponent, title: 'Pruebas',},
+
 ];
