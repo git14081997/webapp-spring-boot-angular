8e7ff16f2645a80c278a48223ea72860b087137d
FranklinRodriguez <frank140897@icloud.com>
2024-01-25 08:38:38 -0600

    se instala angular material

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,10 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
 providers: [
-  provideHttpClient(withFetch()),
-  provideRouter(routes),
-  provideClientHydration()
+    provideHttpClient(withFetch()),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimations()
 ]};
