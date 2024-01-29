62f21603db0fb3cbb0ebf6bd54a5bb7ac0687b38
FranklinRodriguez <frank140897@icloud.com>
2024-01-28 18:36:48 -0600

    orden 0

--- varios/docker/appAngular/up-Dockerfile-httpd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-rm -rf webapp;
-mkdir webapp;
-cp -R ../../frontend/dist/frontend/browser/ ./webapp
-docker build -t miwebserver0 .
-docker run -dit --name container0 -p 80:80 miwebserver0
-exit 0;
