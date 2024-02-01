752cb474fd74fa656101f35021951e098c46916c
FranklinRodriguez <frank140897@icloud.com>
2024-02-01 09:40:32 -0600

    cadena de conexion sql server

---
+++ Ejercicios/src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+
+package org.example;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("");
+		System.out.println("Resultado esperado:");
+		System.out.println("Veces repetido: 3");
+		System.out.println("Número: 8");
+		System.out.println("");
+
+		final int[] NUMEROS = {1,2,2,3,5,5,6,7,7,8,8,8,9};
+
+		final int max = NUMEROS.length;
+
+		int numerosDiferentes = 0;
+
+		int numeroActual = NUMEROS[0];
+
+		for( int i = 0; i < max; i++ ){
+
+			for( int j = 0; j < max; j++ ){
+
+				if(  ){
+					numerosDiferentes++;
+					numeroActual = NUMEROS[]
+				} // if
+
+			} // for2
+
+		} // for1
+
+
+
+	} // static main
+
+} // class
