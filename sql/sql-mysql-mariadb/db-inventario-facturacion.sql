c7093a8cb3736f221a9847cc604bc607bc4c1b8b
FranklinRodriguez <154013584+git14081997@users.noreply.github.com>
2024-02-11 16:09:47 -0600

    se agrega entidad proveedor y se vincula con productos

--- sql/sql-mysql-mariadb/db-inventario-facturacion.sql
+++ sql/sql-mysql-mariadb/db-inventario-facturacion.sql
@@ -11,14 +11,16 @@ create table factura_detalle  (cantidad_producto_vendido integer not null, costo
 create table imagen_producto  (id integer not null auto_increment, archivo longblob, imagen longblob, primary key (id)) engine=innodb charset=utf8mb4 collate=utf8mb4_bin;
 create table ingresos_egresos (egresos decimal(38,2), id integer not null auto_increment, ingresos decimal(38,2), fecha datetime(6), detalle varchar(512), primary key (id)) engine=innodb charset=utf8mb4 collate=utf8mb4_bin;
 create table inventario       (entradas integer, existencia integer, id integer not null auto_increment, producto_id integer, saldo_anterior integer, salidas integer, fecha datetime(6), primary key (id)) engine=innodb charset=utf8mb4 collate=utf8mb4_bin;
-create table producto         (costo_unidad decimal(38,2) not null, existencias integer not null, ganancia decimal(38,2) not null, id integer not null auto_increment, precio_venta decimal(38,2) not null, fecha_adquisicion datetime(6), fecha_modificado datetime(6), nombre varchar(512) not null, primary key (id)) engine=innodb charset=utf8mb4 collate=utf8mb4_bin;
+create table producto         (proveedor_id integer, costo_unidad decimal(38,2) not null, existencias integer not null, ganancia decimal(38,2) not null, id integer not null auto_increment, precio_venta decimal(38,2) not null, fecha_adquisicion datetime(6), fecha_modificado datetime(6), nombre varchar(512) not null, primary key (id)) engine=innodb charset=utf8mb4 collate=utf8mb4_bin;
 create table usuario          (id integer not null auto_increment, pendiente_de_pago decimal(38,2) not null, comentarios varchar(512), apellido varchar(255), apellido_dos varchar(255), nombre varchar(255) not null, nombre_completo varchar(255), nombre_dos varchar(255), primary key (id)) engine=innodb charset=utf8mb4 collate=utf8mb4_bin;
+create table proveedor        (id integer not null auto_increment, nombre varchar(512) not null, direccion varchar(255), primary key (id)) engine=innodb charset=utf8mb4 collate=utf8mb4_bin;
 
 alter table producto        add constraint uk_producto_nombre          unique (nombre);
-alter table cliente_abona   add constraint fk_cliente_abona_usuario    foreign key (usuario_id)  references usuario  (id);
-alter table cliente_abona   add constraint fk_cliente_abona_factura    foreign key (factura_id)  references factura  (id);
-alter table factura         add constraint fk_factura_usuario          foreign key (usuario_id)  references usuario  (id);
-alter table factura_detalle add constraint fk_factura_detalle_factura  foreign key (factura_id)  references factura  (id);
-alter table factura_detalle add constraint fk_factura_detalle_producto foreign key (producto_id) references producto (id);
-alter table inventario      add constraint fk_inventario_producto      foreign key (producto_id) references producto (id);
+alter table producto        add constraint fk_producto_proveedor       foreign key (proveedor_id)  references proveedor (id);
+alter table cliente_abona   add constraint fk_cliente_abona_usuario    foreign key (usuario_id)    references usuario   (id);
+alter table cliente_abona   add constraint fk_cliente_abona_factura    foreign key (factura_id)    references factura   (id);
+alter table factura         add constraint fk_factura_usuario          foreign key (usuario_id)    references usuario   (id);
+alter table factura_detalle add constraint fk_factura_detalle_factura  foreign key (factura_id)    references factura   (id);
+alter table factura_detalle add constraint fk_factura_detalle_producto foreign key (producto_id)   references producto  (id);
+alter table inventario      add constraint fk_inventario_producto      foreign key (producto_id)   references producto  (id);
 
